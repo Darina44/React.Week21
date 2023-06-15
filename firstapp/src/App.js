5acabcfcb344a3e354ba18fdff11db4367f68055
darina bebenina <darinabebenina@yahoo.com>
2023-06-15 15:12:37 +0500

    Chart works

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,15 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import { Chart } from "react-google-charts";
 
-function App() {
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Frontend", 4],
+  ["English", 1],
+  ["Chores", 2],
+  ["Lovely fucking Children", 10],
+  ["Sleep", 7],
+];
+
+export const options = {
+  title: "My Daily Activities",
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
 
